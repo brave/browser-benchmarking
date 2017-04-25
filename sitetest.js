4622054be3bed74a20a60b7237f278677fbbac55
srirambv <sriram@brave.com>
2017-04-25 17:45:54 +0100

    Added test for loading two sites

---
+++ sitetest.js
@@ -0,0 +1,18 @@
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'firefox'
+    }
+};
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function(title) {
+        console.log('Title was: ' + title);
+    })
+    .url('http://www.brave.com')
+    .getTitle().then(function(title) {
+        console.log('Title is: ' + title);
+     })
+    .end();
